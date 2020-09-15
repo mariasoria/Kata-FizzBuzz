19585df5246fb213b6533570a5006c13a4c93b03
Marieta <maria.it3@gmail.com>
2020-09-15 17:38:24 +0100

    Last update

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -4,7 +4,8 @@ public class FizzBuzz {
 
         for(int i = 1; i < number+1; i++)
         {
-            if(((i % 3 == 0) && (i % 5 == 0)) || (Integer.toString(i).contains("3") && Integer.toString(i).contains("5"))) fizzBuzz[i-1] = "FizzBuzz";
+            if(((i % 3 == 0) && (i % 5 == 0)) || (Integer.toString(i).contains("3")
+                    && Integer.toString(i).contains("5"))) fizzBuzz[i-1] = "FizzBuzz";
             else if (i % 3 == 0 || Integer.toString(i).contains("3")) fizzBuzz[i-1] = "Fizz";
             else if (i % 5 == 0 || Integer.toString(i).contains("5")) fizzBuzz[i-1] = "Buzz";
             else fizzBuzz[i-1] = Integer.toString(i);
